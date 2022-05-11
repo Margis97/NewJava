712cd7d6943db8d2bb41f3fd5f3790ddc6b8a335
Margis97 <margaritamorozova197@gmail.com>
2022-05-11 23:55:48 +0300

    working with ArrayList

--- src/Number.java
+++ src/Number.java
@@ -10,21 +10,27 @@ public class Number {
         list.add(100);
         list.add(13);
         System.out.println(list);
+
         Collections.reverse(list);
         System.out.println(list);
+
         list.add(3, 66);
         System.out.println(list);
+
         list.add(5, -8);
         System.out.println(list);
+
         list.remove(0);
         list.remove(1);
         System.out.println(list);
+
         List<Integer> list1 = new ArrayList<Integer>();
         list1.add(26);
         list1.add(-90);
         list1.add(21);
         list1.add(45);
         list1.add(78);
+        
         list.addAll(list1);
         System.out.println(list);
         
